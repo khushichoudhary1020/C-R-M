14703652d33c698a6b4ea1e6d33a15fb56bf04f4
Khushi Choudhary <140972802+khushichoudhary1020@users.noreply.github.com>
2024-02-22 13:49:05 +0530

    Add files via upload

---
+++ filer/leftbar.php
@@ -0,0 +1,22 @@
+ <!-- BEGIN SIDEBAR -->
+  <div class="page-sidebar" id="main-menu">
+    <!-- BEGIN MINI-PROFILE -->
+    <div class="page-sidebar-wrapper scrollbar-dynamic" id="main-menu-wrapper">
+      <div class="user-info-wrapper">
+        <div class="profile-wrapper" style="border:solid #fff 1px;"> 
+        <img src="../assets/img/admin.jpg"  alt="" data-src="../assets/img/admin.jpg" data-src-retina="../assets/img/admin.jpg" class="side-user-img" /> </div>
+        <div class="user-info">
+          <div class="greeting">Welcome</div>
+          <div class="username">Filer</div>
+
+        </div>
+      </div>
+      <!-- END MINI-PROFILE -->
+      <!-- BEGIN SIDEBAR MENU -->
+      <p class="menu-title">BROWSE <span class="pull-right"><a href="#" onclick="location.reload()"><i class="fa fa-refresh"></i></a></span></p>
+
+    <ul>	
+      <li class="start"> <a href="home.php"> <i class="icon-custom-home"></i> <span class="title">Home</span>  </a></li>
+      <li><a href="my-tasks.php"><span></span> My Tasks</a></li>
+      <li><a href="add-visitor.php"><span></span> Add Info</a></li>    
+    </ul>
